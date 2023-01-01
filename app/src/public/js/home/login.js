30133569809ca55d3529d494de7cfdb3ddac965a
WKWKA <ctd0606@naver.com>
2023-01-01 16:58:49 +0900

    dom 을 이용해서 html에 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,15 @@
-console.log('ㅎㅇ')
+
+const id = document.querySelector("#id")
+const password = document.querySelector("#password")
+const loginBtn = document.querySelector("button")
+
+
+loginBtn.addEventListener('click', login)
+
+function login(){
+    const req = {
+        id : id.value,
+        password: password.value
+    }
+    console.log(req)
+}
