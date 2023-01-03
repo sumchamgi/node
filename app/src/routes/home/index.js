f596d31e6be968f39aa11f6a19dcb1f7e56cff0a
WKWKA <ctd0606@naver.com>
2023-01-04 02:45:10 +0900

    패치사용

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const ctrl = require("./home.ctrl")
-router.get("/", ctrl.home)
 
+router.get("/", ctrl.home)
 router.get('/login',ctrl.login)
+
 
 module.exports = router;
